5aa9363978f93f11c838e315d15667f882c2ee63
dorinmoldovan <12948353+dorinmoldovan@users.noreply.github.com>
2018-06-16 13:17:43 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# InventoryApp (Stage1)
+An Inventory App that allows a store to keep track of its inventory of products. The project is part of the "Android Basics Nanodegree by Google - Udacity". (#madewithudacity #googleudacityscholars #fendproject)
---
+++ app/src/main/java/com/androidbasicsnd/android/inventoryapp/InventoryAppActivity.java
@@ -0,0 +1,13 @@
+package com.androidbasicsnd.android.inventoryapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InventoryAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory_app);
+    }
+}
